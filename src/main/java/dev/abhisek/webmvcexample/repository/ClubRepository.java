ae8635e69e991157ea64037432590ef30d346349
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-07 21:28:51 +0530

    added Repository annotation

--- src/main/java/dev/abhisek/webmvcexample/repository/ClubRepository.java
+++ src/main/java/dev/abhisek/webmvcexample/repository/ClubRepository.java
@@ -3,9 +3,11 @@ package dev.abhisek.webmvcexample.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import dev.abhisek.webmvcexample.model.Club;
 
+@Repository
 public interface ClubRepository extends MongoRepository<Club, String> {
     List<Club> findAllByTitle(String title);
 
