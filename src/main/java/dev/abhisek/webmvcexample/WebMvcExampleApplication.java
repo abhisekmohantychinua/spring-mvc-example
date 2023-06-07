59c3d53457549c766ca88ff2d3d2bc2c7cd8bdc5
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-07 20:32:49 +0530

    initial commit

---
+++ src/main/java/dev/abhisek/webmvcexample/WebMvcExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.abhisek.webmvcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvcExampleApplication.class, args);
+	}
+
+}
